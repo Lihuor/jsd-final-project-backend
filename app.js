0086939b67a093430e035b896a102fc0d45b9da9
Lihuor Slot <lihuor@gmail.com>
2023-08-17 08:59:45 +1000

    modified CORS

--- app.js
+++ app.js
@@ -8,13 +8,25 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
 
-
-
 // middleware
 app.use(express.json());
+
+// Configure CORS to allow requests from specified origins
+const allowedOrigins = ['http://localhost:3000', 'https://mern-budget-management-final-project.onrender.com'];
+
+// Check if the environment is production, and then allow the deployed frontend URL
+if (process.env.NODE_ENV === 'production') {
+    allowedOrigins.push('https://mern-budget-management.onrender.com'); 
+}
+
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://mern-budget-management-final-project.onrender.com'], 
+    origin: allowedOrigins,
+    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 }));
+
+// Handle preflight requests
+app.options('*', cors());
+
 
 // routes
 readdirSync('./routes').map((r) => app.use('/api/v1', require(`./routes/${r}`)));
