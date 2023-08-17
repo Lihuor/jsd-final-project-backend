ff63524e5c869b1dfff6c5c57e92a9bd218b28b9
Lihuor Slot <lihuor@gmail.com>
2023-08-17 11:40:53 +1000

    fixing allowOrigins

--- app.js
+++ app.js
@@ -12,12 +12,18 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // Configure CORS to allow requests from specified origins
-const allowedOrigins = [ 'http://localhost:3000', 'https://mern-budget-management-final-project.onrender.com'];
+const allowedOrigins = [
+    'http://localhost:3000', // For local development
+    'https://mern-budget-management-final-project.onrender.com', // Front-end URL
+    'https://mern-budget-management.onrender.com' // Back-end URL
+];
 
 // Check if the environment is production, and then allow the deployed frontend URL
 if (process.env.NODE_ENV === 'production') {
+    allowedOrigins.push('https://mern-budget-management-final-project.onrender.com');
     allowedOrigins.push('https://mern-budget-management.onrender.com'); 
 }
+
 
 app.use(cors({
     origin: allowedOrigins,
